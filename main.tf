b2c64a7dc65235cb366070c54b0da21b0a234e82
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2021-03-09 23:29:50 -0800

    adding day2 security group

--- main.tf
+++ main.tf
@@ -7,10 +7,6 @@ provider "aws" {
 
 //--------------------------------------------------------------------
 // Modules
-
-
-//--------------------------------------------------------------------
-// Modules
 module "ec2_instance" {
   source  = "app.terraform.io/hc-se-demo-chintan/ec2-instance/aws"
   version = "2.19.0"
@@ -20,7 +16,7 @@ module "ec2_instance" {
   key_name = "cgosalia-aws-key"
   name = "cgosalia-vault-test1"
   subnet_id = "subnet-024d03616522c6a61"
-  vpc_security_group_ids = tolist(module.security_group.this_security_group_id)
+  vpc_security_group_ids = tolist(module.security_group.this_security_group_id, module.security_group_2.this_security_group_id)
 }
 
 module "elb" {
@@ -43,5 +39,19 @@ module "security_group" {
   protocol = "TCP"
   rule_description = "vault security group"
   to_port = 8201
+  vpc_id = "vpc-00e43c5433b4eb92c"
+}
+
+module "security_group_2" {
+  source  = "app.terraform.io/hc-se-demo-chintan/security-group/aws"
+  version = "3.28.0"
+
+  cidr_block = ["0.0.0.0/0"]
+  description = "day2 security group"
+  from_port = 90
+  name = "cgosalia-day2-sg"
+  protocol = "UDP"
+  rule_description = "day 2 security group"
+  to_port = 100
   vpc_id = "vpc-00e43c5433b4eb92c"
 }
