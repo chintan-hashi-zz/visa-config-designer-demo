0d785ebd705212e6f2f95c46b6a5f90f1c462945
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2021-03-09 16:52:03 -0800

    adding security group

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  =  "us-west-1"
-}
+//--------------------------------------------------------------------
+// Variables
 
 
-module "ec2_instance1" {
+
+//--------------------------------------------------------------------
+// Modules
+module "ec2_instance" {
   source  = "app.terraform.io/hc-se-demo-chintan/ec2-instance/aws"
   version = "2.19.0"
 
@@ -13,17 +14,18 @@ module "ec2_instance1" {
   key_name = "cgosalia-aws-key"
   name = "cgosalia-vault-test1"
   subnet_id = "subnet-024d03616522c6a61"
-  vpc_security_group_ids = ["sg-049cb0ce979def91b"]
+  vpc_security_group_ids = "${module.security_group.this_security_group_id}"
 }
 
-module "ec2_instancer2" {
-  source  = "app.terraform.io/hc-se-demo-chintan/ec2-instance/aws"
-  version = "2.19.0"
+module "security_group" {
+  source  = "app.terraform.io/hc-se-demo-chintan/security-group/aws"
+  version = "3.19.0"
 
-  ami = "ami-059fd73d4594fa21c"
-  instance_type = "t2.medium"
-  key_name = "cgosalia-aws-key"
-  name = "cgosalia-vault-test1"
-  subnet_id = "subnet-024d03616522c6a61"
-  vpc_security_group_ids = ["sg-049cb0ce979def91b"]
+  create = "true"
+  description = "testing security group"
+  ingress_cidr_blocks = "10.10.0.0/16"
+  ingress_rules = "https-tcp"
+  ingress_with_cidr_blocks = "[     {       from_port   = 8080       to_port     = 8090       protocol    = "tcp"       description = "User-service ports"       cidr_blocks = "10.10.0.0/16"     } ]"
+  name = "test-sg-cgosalia"
+  vpc_id = "vpc-00e43c5433b4eb92c"
 }
