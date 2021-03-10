ba0019831320c256a727f2524f515e870c8774d5
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2021-03-10 08:03:32 -0800

    visa opearations 1

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ module "ec2_instance" {
   ami = "ami-059fd73d4594fa21c"
   instance_type = "t2.micro"
   key_name = "cgosalia-aws-key"
-  name = "cgosalia-vault-test-1"
+  name = "cgosalia-visa-vault-test1"
   subnet_id = "subnet-024d03616522c6a61"
-  vpc_security_group_ids = concat(module.security_group.this_security_group_id, module.security_group_2.this_security_group_id)
+  vpc_security_group_ids = tolist(module.security_group.this_security_group_id)
 }
 
 module "elb" {
@@ -26,7 +26,7 @@ module "elb" {
 
   instances = tolist(module.ec2_instance.id)
   internal = "false"
-  name = "cgosalia-vault-elb"
+  name = "cgosalia-visa-elb-vault"
 }
 
 module "security_group" {
@@ -34,33 +34,11 @@ module "security_group" {
   version = "3.28.0"
 
   cidr_block = ["0.0.0.0/0"]
-  description = "cgosalia vault visa test security group"
+  description = "Visa Day1 operations security group"
   from_port = 80
-  name = "cgosalia-vault-sg-test"
+  name = "cgosalia-visa-day1-sg-vault"
   protocol = "TCP"
-  rule_description = "cgosalia vault visa test security group"
+  rule_description = "Visa Day1 operations security group"
   to_port = 8201
   vpc_id = "vpc-00e43c5433b4eb92c"
-}
-
-module "security_group_2" {
-  source  = "app.terraform.io/hc-se-demo-chintan/security-group/aws"
-  version = "3.28.0"
-
-  cidr_block = ["0.0.0.0/0"]
-  description = "visa day 2 operation security group"
-  from_port = 10
-  name = "cgosalia-visa-day2-sg"
-  protocol = "UDP"
-  rule_description = "visa day 2 operation security group"
-  to_port = 100
-  vpc_id = "vpc-00e43c5433b4eb92c"
-}
-
-module "ebs_volume" {
-  source  = "app.terraform.io/hc-se-demo-chintan/ebs-volume/aws"
-  version = "1.4.0"
-
-  instance_id = module.ec2_instance.id[0]
-  size = 30
 }
