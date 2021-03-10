93f867683a4aa8518a7785ce47c0b9052a5d9e22
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2021-03-09 16:58:42 -0800

    adding security group

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "security_group" {
   create = "true"
   description = "testing security group"
   ingress_cidr_blocks = "10.10.0.0/16"
-  ingress_rules = "https-tcp"
+  ingress_rules = [ "https-tcp" ]
   ingress_with_cidr_blocks = [
 	{
 	      	from_port   = 8080
