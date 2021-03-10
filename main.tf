efc8aa6704d2c0a2dd8c0642f0b54303636c3f21
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2021-03-09 20:16:18 -0800

    using new security group module

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ provider "aws" {
 
 //--------------------------------------------------------------------
 // Modules
+
+
+//--------------------------------------------------------------------
+// Modules
 module "ec2_instance" {
   source  = "app.terraform.io/hc-se-demo-chintan/ec2-instance/aws"
   version = "2.19.0"
@@ -16,26 +20,19 @@ module "ec2_instance" {
   key_name = "cgosalia-aws-key"
   name = "cgosalia-vault-test1"
   subnet_id = "subnet-024d03616522c6a61"
-  vpc_security_group_ids = "${module.security_group.this_security_group_id}"
+  vpc_security_group_ids = "module.security_group.this_security_group_id"
 }
 
 module "security_group" {
   source  = "app.terraform.io/hc-se-demo-chintan/security-group/aws"
-  version = "3.23.0"
+  version = "3.26.0"
 
-  create = "true"
-  description = "testing security group"
-  ingress_cidr_blocks = ["10.10.0.0/16"]
-  ingress_rules = ["https-tcp"]
-  ingress_with_cidr_blocks = [
-	{
-	      	from_port   = 8080
-		to_port     = 8090
-		protocol    = "tcp"
-		description = "User-service ports"
-		cidr_blocks = "10.10.0.0/16"
-	}
-  ]
-  name = "test-sg-cgosalia"
+  cidr_block = ["0.0.0.0/0"]
+  description = "TLS traffic"
+  from_port = 8080
+  name = "tls-sg-cgosalia"
+  Protocol = "TCP"
+  rule_description = "allowing TLS traffic"
+  to_port = 8090
   vpc_id = "vpc-00e43c5433b4eb92c"
 }
